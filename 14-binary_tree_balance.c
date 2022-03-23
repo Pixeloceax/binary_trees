93af4a2b4edd55dfa131c915676c7e5e52932c20
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-03-23 15:41:05 +0000

    add task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,51 @@
 #include "binary_trees.h"
+/**
+ *binary_tree_balance - function
+ *@tree: v
+ *Return: int
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	
+	if (!tree)
+	{
+		return (0);
+	}
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+
+/**
+ *binary_tree_height - function
+ *@tree: v
+ *Return: size_t
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left_branche = 0;
+	size_t height_right_branche = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left)
+	{
+		height_left_branche = binary_tree_height(tree->left);
+	}
+
+	if (tree->right)
+	{
+		height_right_branche = binary_tree_height(tree->right);
+	}
+
+	if (height_left_branche > height_right_branche)
+	{
+		return (1 + height_left_branche);
+	}
+
+	else
+	{
+		return (1 + height_right_branche);
+	}
 }
